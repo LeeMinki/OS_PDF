8a79095259b68c03f99044610092041c90086be2
LeeMinki <curyowo@gmail.com>
2019-04-30 17:55:00 +0900

    Add file browser

--- app/src/main/java/com/example/os_pdf/MainActivity.java
+++ app/src/main/java/com/example/os_pdf/MainActivity.java
@@ -1,13 +1,149 @@
 package com.example.os_pdf;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.content.Context;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_STORAGE = 1111;
+    private String mFileName;
+    private ListView lvFileControl;
+    private Context mContext = this;
+
+    private List<String> lItem = null;
+    private List<String> lPath = null;
+    private String mRoot = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private TextView mPath;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
+        mPath = (TextView) findViewById(R.id.tvPath);
+        lvFileControl = (ListView) findViewById(R.id.lvFileControl);
+        getDir(mRoot);
+
+        lvFileControl.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                File file = new File(lPath.get(position));
+
+                if (file.isDirectory()) {
+                    if (file.canRead())
+                        getDir(lPath.get(position));
+                    else {
+                        Toast.makeText(mContext, "No files in this folder.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    mFileName = file.getName();
+                    Log.i("Test", "ext:" + mFileName.substring(mFileName.lastIndexOf('.') + 1, mFileName.length()));
+                }
+            }
+        });
+    }
+    // whoami??????
+    private void getDir(String dirPath) {
+        mPath.setText("Location: " + dirPath);
+        Log.i("Test", "mPath:" + mPath.getText());
+
+        lItem = new ArrayList<String>();
+        lPath = new ArrayList<String>();
+
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+
+        if (!dirPath.equals(mRoot)) {
+            lItem.add("../"); //to parent folder
+            lPath.add(f.getParent());
+        }
+
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            lPath.add(file.getAbsolutePath());
+
+            if (file.isDirectory())
+                lItem.add(file.getName() + "/");
+            else
+                lItem.add(file.getName());
+        }
+
+        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lItem);
+        lvFileControl.setAdapter(fileList);
+    }
+
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 다시 보지 않기 버튼을 만드려면 이 부분에 바로 요청을 하도록 하면 됨 (아래 else{..} 부분 제거)
+            // 처음 호출시엔 if()안의 부분은 false로 리턴 됨 -> else{..}의 요청으로 넘어감
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("알림")
+                        .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
+                        .setNeutralButton("설정", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intent.setData(Uri.parse("package:" + getPackageName()));
+                                startActivity(intent);
+                            }
+                        })
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_STORAGE:
+                for (int i = 0; i < grantResults.length; i++) {
+                    // grantResults[] : 허용된 권한은 0, 거부한 권한은 -1
+                    if (grantResults[i] < 0) {
+                        Toast.makeText(MainActivity.this, "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                // 허용했다면 이 부분에서..
+
+                break;
+        }
     }
 }
