3c1760b722c01a302036121a656afc243364a3a8
weehyerin <weehyerin>
2019-05-01 20:53:22 +0900

    Add parsing code

--- app/src/main/java/com/MoP/os_pdf/PdfActivity.java
+++ app/src/main/java/com/MoP/os_pdf/PdfActivity.java
@@ -7,9 +7,15 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.text.Layout;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import com.tom_roush.pdfbox.cos.COSName;
 import com.tom_roush.pdfbox.pdmodel.PDDocument;
@@ -26,11 +32,21 @@ import com.tom_roush.pdfbox.util.PDFBoxResourceLoader;
 import java.io.File;
 import java.io.IOException;
 
+//import java.io.FileInputStream;
+//import java.io.InputStream;
+//
+//import opennlp.tools.sentdetect.SentenceDetectorME;
+//import opennlp.tools.sentdetect.SentenceModel;
+
 public class PdfActivity extends Activity {
     private String filePath;
     private PDFTextStripper pdfStripper;
     private String text;
     private TextView resultTextView;
+    List sentences = new ArrayList();
+    BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+    private int count = 0;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,9 +54,46 @@ public class PdfActivity extends Activity {
         setContentView(R.layout.pdf_layout);
         PDFBoxResourceLoader.init(getApplicationContext());
         Intent intent = getIntent();
+
         filePath = intent.getExtras().getString("fileName");
 //        viewPdf(fileName);
         extractText(filePath);
+        Button button = (Button)findViewById(R.id.button);
+        Button button1 = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(0<=count && count < sentences.size()){
+                    count++;
+
+                    if(count<0)
+                        count = 0;
+                    if(count>sentences.size())
+                        count = sentences.size();
+                    resultTextView.setText(sentences.get(count).toString());
+                }
+
+
+            }
+        });
+        button1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(0<=count && count < sentences.size()){
+                    count--;
+
+
+                    if(count<0)
+                        count = 0;
+                    if(count>sentences.size())
+                        count = sentences.size();
+                    resultTextView.setText(sentences.get(count).toString());
+                }
+
+
+            }
+        });
+
         extractImage(filePath);
     }
 //    private void viewPdf(String pdfFileName) {
@@ -55,15 +108,40 @@ public class PdfActivity extends Activity {
         try {
             PDDocument document = PDDocument.load(file);
             pdfStripper = new PDFTextStripper();
-            text = pdfStripper.getText(document);
             resultTextView = findViewById(R.id.textView);
+            text = pdfStripper.getText(document);
+
             resultTextView.setMovementMethod(ScrollingMovementMethod.getInstance());
-            resultTextView.setText(text);
+            iterator.setText(text);
+
+//            String simple = "[.?!]";
+//            String[] splitString = (text.split(simple));
+//            for (String string : splitString)
+//                sentences.add(string);
+
+
+
+            int lastIndex = iterator.first();
+            while (lastIndex != BreakIterator.DONE) {
+                int firstIndex = lastIndex;
+                lastIndex = iterator.next();
+
+                if (lastIndex != BreakIterator.DONE) {
+                    String sentence = text.substring(firstIndex, lastIndex);
+
+                    //resultTextView.setText("sentence = " + sentence);
+                    sentences.add(sentence);
+                }
+            }
+
+            //resultTextView.setText(text);
+            resultTextView.setText(sentences.get(0).toString());
             document.close();
         } catch (Exception e) {
             //...
         }
     }
+
 
     private void extractImage(String pdfFilePath) {
         LinearLayout layout = (LinearLayout)findViewById(R.id.linearLayout);
